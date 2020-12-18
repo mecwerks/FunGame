a39a3aa6eb18b9190990342996883daa4604beca
Marcus Collins <mecwerks@gmail.com>
2020-12-17 23:46:29 -0500

    Check for all enemies in list for the floor

--- Assets/Scripts/FloorManager.cs
+++ Assets/Scripts/FloorManager.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 using Mirror;
 
 public class FloorManager : NetworkBehaviour {
-    public Damageable enemy;
+    public Damageable[] enemies;
     public FloorChangePortal portal;
 
     void Update() {
         if (isServer) {
-            if (enemy.health <= 0) {
+            bool enemyAlive = false;
+
+            foreach (var enemy in enemies) {
+                if (enemy.health > 0) {
+                    enemyAlive = true;
+                    break;
+                }
+            }
+
+            if (!enemyAlive) {
                 portal.active = true;
-            } else {
-                portal.active = false;
             }
         }
     }
